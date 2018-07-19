6e0193004281ee52f89e76d6316420ac5347344b
Nathan <zhanghengzhezy@gmail.com>
2018-07-19 15:16:48 -0500

    final submission

--- crypto_settlement/main.py
+++ crypto_settlement/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 import datetime as dt
 import time
 import requests
+import logging
+import sys
 
 pd.options.mode.chained_assignment = None
 
@@ -27,6 +29,10 @@ def iso_converter(s):
 
 def data_retriever(prod_id='BTC-USD'):
     """
+    This function will connect to GDAX api and receive live trading data of
+    given product. The trade data of each product include: trade_id, time,
+    price, and quantity (size).
+
     args:
         prod_id: product ID, eg. BTC-USD, ETH-USD
 
@@ -47,61 +53,18 @@ def data_retriever(prod_id='BTC-USD'):
     return df
 
 
-"""
-def select_data(data, delay=20, first_exec=True):
-    
-    This function will select the data in accord to time window.
-
-    args:
-        data: all data points to select from
-        delay: time delayed of received data
-        first_exec: whether it is first execution
-
-    return: trades within the time window
-    
-    # print('select_data received table:')
-    # print(data)
-
-    # Get current UTC time
-    utc_now = dt.datetime.utcnow()
-    # print('utc_now: '+str(utc_now))
-
-    # Get current minute
-    current_minute = dt.datetime(utc_now.year, utc_now.month, utc_now.day,
-                                 utc_now.hour, utc_now.minute)
-
-    # Get desired time window
-    window_end = current_minute - dt.timedelta(seconds=delay)
-    window_start = current_minute - dt.timedelta(seconds=delay+10)
-    # print('window_end: '+str(window_end))
-    # print('window_start: ' + str(window_start))
-
-    # Convert string to datetime object
-    data_time = data['time'].apply(iso_converter)
-
-    # Create mask for data selection
-    if first_exec:
-        # If first execute, select all data prior to delay
-        mask = (data_time < window_end)
-    else:
-        # Select data within the time window
-        mask = (data_time < window_end) & (data_time > window_start)
-
-    trades_selected = data[mask]
-
-    return trades_selected
+def parties_assigner(n_parties, table1, table2=None):
     """
+    This function will assign buyers and sellers to each trade.
 
-
-def parties_assign(n_parties, table1, table2=None):
-    """
-    Assign buyers and sellers to each trade
+    The assignment is simulated by randomly assigning the trades to two out of
+    10 or more (adjustable in parameter n_parties) parties as long and short
+    sides.
 
     args:
         n_parties: total number of counter parties
-        table: the table of trades
+        table: the table of trades with assigned counter parties.
     """
-    # print('parties_assign received table:')
 
     parties_list = []
 
@@ -114,13 +77,16 @@ def parties_assign(n_parties, table1, table2=None):
 
         if len(new_rows) > 0:
 
+            print('Newly added %.2f trades.' % len(new_rows))
+
             for i in np.arange(len(new_rows)):
-                parties_list.append(np.random.choice(10, 2, replace=False))
+                parties_list.append(np.random.choice(n_parties, 2,
+                                                     replace=False))
 
             parties_list = np.array(parties_list)
 
-            new_rows['Buyer'] = parties_list[:, 0]
-            new_rows['Seller'] = parties_list[:, 1]
+            new_rows['long'] = parties_list[:, 0]
+            new_rows['short'] = parties_list[:, 1]
 
             table1 = pd.concat([table1, new_rows])
 
@@ -130,38 +96,110 @@ def parties_assign(n_parties, table1, table2=None):
 
         parties_list = np.array(parties_list)
 
-        table1['Buyer'] = parties_list[:, 0]
-        table1['Seller'] = parties_list[:, 1]
+        table1['long'] = parties_list[:, 0]
+        table1['short'] = parties_list[:, 1]
 
     return table1.sort_index(ascending=False)
 
 
-def vwap_settlement(data, window=3600):
+def settlement_calculator(trade_data, prod_id, window_size=3600):
     """
-    Calculate the volume weighted average price (VWAP) in the moving window of
-    given size
+    This function will calculate the volume weighted average price (VWAP) in
+    the moving window of given size and calculate settlement obligations of
+    each party.
+
+    The settlement price is calculated as colume-weighted average price (VWAP)
+    settlement value per product, in USD. The default window size used for
+    settlement price calculation is one hour (or 3600 seconds).
+
+    The settlement obligation is the difference between the settlement price
+    and latest trade price, multiplied by the quantity. Positive settlement
+    obligation means the participant owes money; negative settlement means
+    the participant has right to claim money.
 
     args:
-        data: the data should contain information of product, price, volume,
-        time, buyer & sellers.
-        window: the moving window which the function use to calculate volume
+        trade_data: the data should contain information of product, price,
+        volume, time, buyer & sellers.
+        prod_id: the product id, eg. BTC-USD, ETH-USD
+        window_size: the moving window size used to calculate volume
         weighted average price.
 
-    return: settlement table
+    return: settlements table which has settlement information of each party.
     """
-    data['trade_net_worth'] = data['price'] * data['size']
 
-    settlements = []
+    # Take the data of lastest hour
+    last_hr_data = trade_data[trade_data['time'] > dt.datetime.utcnow() -
+                              dt.timedelta(seconds=window_size)]
+
+    # Calculate worth of each trade
+    last_hr_data['price'] = last_hr_data['price'].astype(float)
+    last_hr_data['size'] = last_hr_data['size'].astype(float)
+    last_hr_data['trade_worth'] = last_hr_data['price'] * last_hr_data['size']
+
+    # Calculate total worth of crypto in all trade of each party
+    total_worth = last_hr_data.groupby('long')['trade_worth'].sum() - \
+                  last_hr_data.groupby('short')['trade_worth'].sum()
+
+    # Calculate total quantity of crypto in all trade for each party
+    total_quantity = last_hr_data.groupby('long')['size'].sum() - \
+                     last_hr_data.groupby('short')['size'].sum()
+
+    # Calculate volume-weighted average price settlement value
+    settlements = pd.DataFrame(total_worth / total_quantity,
+                               columns=['vwap_value'])
+
+    # Assign currency USD
+    settlements['currency'] = prod_id[-3:]
+
+    # Insert total quantity calculated earlier
+    settlements['quantity'] = total_quantity
+
+    # Rename index
+    settlements.index.names = ['party']
+
+    # Retrieve latest trade price
+    r = requests.get('https://api.gdax.com/products/{}/ticker'.format(prod_id))
+    settlements['current_price'] = float(r.json()['price'])
+
+    # Calculate settlement obligation of each party
+    settlements['settlement_obligation'] = (settlements['vwap_value'] -
+                                            settlements['current_price']) * \
+                                           settlements['quantity']
 
     return settlements
+
+
+def balance_calculator(bal, set, set_old=None):
+    """
+    Update the participants' balances in the balances table from the previous
+    settlement run
+
+    args:
+        bal: the balances from last execution
+        set: the settlemments from this execution
+        set_old: the settlements from last execution
+
+    return: updated balances reflecting new settlements
+    """
+
+    if set_old is None:
+        bal['current_balance'] = bal['current_balance'] - \
+                                 set['settlement_obligation']
+    else:
+        bal['current_balance'] = bal['current_balance'] - \
+                                 (set['settlement_obligation'] -
+                                  set_old['settlement_obligation'])
+
+    return bal
 
 
 if __name__ == '__main__':
     # Loop will run until keyboard interrupt (Ctrl-C)
     try:
         # Set initial balance for counterparties
-        balances = pd.DataFrame({'Party': range(10),
-                                 'Balance': 100000}).set_index('Party')
+        balances = pd.DataFrame({'party': range(10),
+                                 'initial_balance': 100000,
+                                 'current_balance': 100000}).set_index('party')
         start_time = time.time()
 
         # 1st data retrieval
@@ -169,8 +207,23 @@ if __name__ == '__main__':
         trades_ETH = data_retriever('ETH-USD')
 
         # Assign trades to counter parties
-        trades_BTC = parties_assign(10, trades_BTC)
-        trades_ETH = parties_assign(10, trades_ETH)
+        trades_BTC = parties_assigner(10, trades_BTC)
+        trades_ETH = parties_assigner(10, trades_ETH)
+
+        # Calculate settlements
+        settlements_BTC = settlement_calculator(trades_BTC, 'BTC-USD')
+        settlements_ETH = settlement_calculator(trades_ETH, 'ETH-USD')
+
+        print('The initial settlements:')
+        print(settlements_BTC)
+        print(settlements_ETH)
+
+        # Update the balances with regard to BTC and ETH trades
+        balances = balance_calculator(balances, settlements_BTC)
+        balances = balance_calculator(balances, settlements_ETH)
+
+        print('The initial balances:')
+        print(balances)
 
         # Trigger execution every preset time interval
         elapsed_time = time.time() - start_time
@@ -181,16 +234,34 @@ if __name__ == '__main__':
             # The start time for each minutely execution
             start_time = time.time()
 
+            # Keep old settlements tables
+            settlements_BTC_old = settlements_BTC.copy()
+            settlements_ETH_old = settlements_ETH.copy()
+
             # Retrieve existing trades up to 1000
             new_trades_BTC = data_retriever('BTC-USD')
             new_trades_ETH = data_retriever('ETH-USD')
 
             # Assign trades to counter parties
-            trades_BTC = parties_assign(10, trades_BTC, new_trades_BTC)
-            trades_ETH = parties_assign(10, trades_ETH, new_trades_ETH)
+            trades_BTC = parties_assigner(10, trades_BTC, new_trades_BTC)
+            trades_ETH = parties_assigner(10, trades_ETH, new_trades_ETH)
 
-            # settlements = vwap_settlement(trades_BTC)
-            # settlements = vwap_settlement(trades_BTC)
+            # Calculate settlements
+            settlements_BTC = settlement_calculator(trades_BTC, 'BTC-USD')
+            settlements_ETH = settlement_calculator(trades_ETH, 'ETH-USD')
+
+            print('The updated settlements:')
+            print(settlements_BTC)
+            print(settlements_ETH)
+
+            # Update the balances with regard to BTC and ETH trades
+            balances = balance_calculator(balances, settlements_BTC,
+                                          settlements_BTC_old)
+            balances = balance_calculator(balances, settlements_ETH,
+                                          settlements_ETH_old)
+
+            print('The updated balances:')
+            print(balances)
 
             # Trigger execution every preset time interval
             elapsed_time = time.time() - start_time
